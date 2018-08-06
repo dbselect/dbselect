7ca462215ed0285d4b7e3bf4c025662c1295ce0e
dbselect <42153704+dbselect@users.noreply.github.com>
2018-08-07 00:24:42 +0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dbselect",
+    version="0.0.1",
+    author="mmoiz",
+    author_email="moizmuhammad@yahoo.com",
+    description="Generate SQL select statement for given parameters",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dbselect/dbselect",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
